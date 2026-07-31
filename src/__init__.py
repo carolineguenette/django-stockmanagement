a08eb73ebea3aa97ebf62573154b430d2d34767a
Caroline Guénette <cguenette@telus.net>
2026-07-31 02:48:48 -0400

    SD-50 Automate tests (pytest) on master and create dynamic badgets (version and test coverage) (#1)
    * SD-50 Try JSON directly and orphan branch
    
    * SD50 - Add dynamic badge for the version
    
    * SD-50 Fix Version badge
    
    * SD-50 Fix: clean badges branch
    
    * SD-50 Change version string to test dynamic badgets
    
    * SD-50 Fix COLOR_COV
    
    * SD-50 Add conditionnal badge update (if push on master)

---
+++ src/__init__.py
@@ -0,0 +1,24 @@
+
+"""
+Jalons du projet :
+------------------
+0.0.x : Phase de développement initiale (pré-POC)
+0.1.x : POC (Proof of Concept)
+        Validation de l'architecture multi-tenant et de l'isolation
+        des données par entreprise.
+0.2.x : MVP (Minimum Viable Product)
+        Mise en œuvre de la valeur métier brute : gestion sécurisée
+        des stocks et des mouvements d'inventaire.
+0.3.x à 0.9.x : Next
+        Ajouts incrémentiels de couches de fonctionnalités.
+        Un tag (ex: 0.3.0-notifications) sera utilisé pour décrire
+        la fonctionnalité majeure implémentée.
+1.0   : V1
+        Première version finale incluant tous les éléments
+        du cahier des charges.
+> 2.0: Future (VX)
+        Versions futures pour les fonctionnalités prévues durant la conception
+        mais non prévues dans le développement actuel.
+"""
+__version__ = "0.0.1 (vers le POC)"
+
