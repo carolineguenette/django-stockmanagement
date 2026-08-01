dd653fa6e8a1214b969a8e6c7e7da5c33f7d954f
Caroline Guénette <cguenette@telus.net>
2026-07-31 20:42:37 -0400

    SD-52 Clean settings.py, move custom context in core

---
+++ src/core/context_processors.py
@@ -0,0 +1,7 @@
+from django.conf import settings
+
+def project_context(request):
+    """Injecte des variables globales dans l'ensemble des gabarits HTML."""
+    return {
+        "PROJECT_AUTHOR": getattr(settings, "PROJECT_AUTHOR", ""),
+    }
